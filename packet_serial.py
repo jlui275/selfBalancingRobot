7c1a7d4bd94320afe172de14a5c239cc4ec2281a
root <root@beaglebone.localdomain>
2019-11-21 20:48:13 +0000

    add motor driving

--- packet_serial.py
+++ packet_serial.py
@@ -1,25 +1,57 @@
 from roboclaw import Roboclaw
 from time import sleep
+from roboclaw import Roboclaw
+from time import sleep
 
+class RoboClawAdvance:
+   def __init__(self):
+        self.PWM_MAX = 127
+        self.address = 0x80
+        self.roboclaw = Roboclaw("/dev/ttyS1", 9600)
+        self.roboclaw.Open()
+    def MotorDrive1(self,speed);
+        if speed < 0:
+            # Reverse
+            pwm = int(PWM_MAX * speed)
+            if pwm > PWM_MAX:
+                pwm = PWM_MAX
+            roboclaw.BackwardM1(address,pwm)
+        else:
+            # Forward / stoppe
+            pwm = int(PWM_MAX * speed)
+            if pwm > PWM_MAX:
+                pwm = PWM_MAX
+            roboclaw.ForwardM1(address,pwm)
+    def MotorDrive2(self,speed):
+          if speed < 0:
+            # Reverse
+            pwm = int(PWM_MAX * speed)
+            if pwm > PWM_MAX:
+                pwm = PWM_MAX
+            roboclaw.BackwardM2(address,pwm)
+        else:
+            # Forward / stoppe
+            pwm = int(PWM_MAX * speed)
+            if pwm > PWM_MAX:
+                pwm = PWM_MAX
+            roboclaw.ForwardM2(address,pwm)
 if __name__ == "__main__":
-    
     address = 0x80
     roboclaw = Roboclaw("/dev/ttyS1", 9600)
     roboclaw.Open()
     
     while True:
-        
-     
-        
-        roboclaw.ForwardM1(address,127)
+       
         sleep(2)
         roboclaw.ForwardM1(address,0)
-        sleep(2)
-        roboclaw.ForwardM1(address,-1)
+        roboclaw.ForwardM(address,0)
         sleep(2)
         
         
         
     
+    
+
+
     
 
